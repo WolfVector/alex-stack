2323828f397a5034141524204643f134fde3ba61
WolfVector <angry_programmer@hotmail.com>
2023-07-10 21:18:17 -0600

    frontend test api

---
+++ frontend/src/routes/api/auth/+server.js
@@ -0,0 +1,21 @@
+import { json } from '@sveltejs/kit'
+
+export async function GET(event) {
+  const cookie = event.cookies.get("connect.sid")
+  console.log(cookie)
+  
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  if (cookie) {
+    console.log("siii")
+    headers.append('Cookie', `connect.sid=${cookie}`);
+  }
+  let res = await fetch(`http://localhost:3000/get-user`,{
+      method: "GET",
+      headers
+  });
+  let data = await res.json();
+  console.log(data);
+
+  return json({ ok: true })
+}
